83344c200b0601e06a63612b2e89fc3078363bf6
jscott <jscott@JSCOTT-JZB8SY1>
2018-04-13 18:03:09 -0500

    jQuery Loops

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ WEBSOCKET.onmessage = onMessage;
 WEBSOCKET.onerror = onError;
 
 function onOpen(evt) {
+    //alert('Sending login message on opened connection');
     // Send the login message
     console.log('Sending login message on opened connection');
     var msg = {
@@ -31,13 +32,24 @@ function onOpen(evt) {
 }
 
 function onMessage(evt) {
-   
+    var htmlOutput = "~";
     console.log('Received message: ' + evt.data);
     var msg = JSON.parse(evt.data);
     switch (msg.meta.command) {
     case 'Login':
         {
-            onLogin(msg);
+                onLogin(msg);
+                
+                $.each(msg, function (index, i) {
+                    if (index == 'data') {
+                        $.each(msg.data, function (indexSecond, i) {
+                            $('#header').html("Welcome, " + i.username);
+                        });
+                    }
+                    htmlOutput += i.command + " ";
+                    htmlOutput += i.status + " ";
+                });
+                $('#dvDemo').html(htmlOutput);
             break;
         }
 
@@ -81,3 +93,49 @@ function onLogin(msg) {
 function onError(msg) {
     
 }
+
+
+function search(msg)
+{
+    WEBSOCKET.onopen = onOpen;
+    var CZ17Show = "~";
+    var symbols = $('#searchText').val();
+    var msg = {
+        meta: { command: 'ChartSnap',
+                requestId: 9},
+        data: {
+            expression: '@CZ17',
+            limit: 1,
+            interval: 'MINUTE',
+            intervalCount: 5
+        }
+    };
+    WEBSOCKET.send(JSON.stringify(msg));
+
+    var results = "";
+    WEBSOCKET.onmessage = function (event) {
+        fillList(event);
+        //alert(event);
+    };
+    //alert(symbols);
+}
+
+function fillList(event) {
+    var html = "";
+    $.each(event, function (index1, i1) {
+        if (index1 == 'data') {
+
+            var open = JSON.parse(i1);
+            $.each(open, function (index2, open) {
+                if (index2 == 'data') {
+                    var x = open.data.Open;
+                    $.each(x, function (index3, x2) {
+                        var y = x2;
+                    });
+                    html += open.data + " ";
+                }
+            });
+        }
+        $('#listQuotes').html(html);
+    });
+};
